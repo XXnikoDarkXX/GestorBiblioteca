259e8d9c27202ec4c716a5e54bc9eeeb98ad2e2c
XXnikoDarkXX <nicocheto1@gmail.com>
2020-05-15 18:03:26 +0200

    Constructor y getter Persona/Usuario
    He creado variables internas como Constructores

--- GestorBibliotecaProyecto/src/main/java/objetosVivos/Persona.java
+++ GestorBibliotecaProyecto/src/main/java/objetosVivos/Persona.java
@@ -10,5 +10,73 @@ package objetosVivos;
  * @author nicoc
  */
 public class Persona {
+    private String nombre;//nombre de la persona
+    private String apellido;//apellido de la persona
+    private byte edad;//Edad de la persona
+    private String dni;//Dni de la persona
+    
+    /**
+     * Constructor de Persona con todos sus parametros
+     * @param n nombre de la persona
+     * @param a apellido de la persona
+     * @param e edad de la persona
+     * @param d dni de la persona
+     */
+    public Persona(String n,String a, byte e,String d){
+        this.nombre=n;
+        this.apellido=a;
+        this.edad=e;
+        this.dni=d;
+    }
+
+    /**
+     * Getter de nombre
+     * @return el nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Getter de apellido
+     * @return el Apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * Getter de edad
+     * @return la edad
+     */
+    public byte getEdad() {
+        return edad;
+    }
+
+    /**
+     * Setter de nombre
+     * @param nombre nombre a modificar
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    /**
+     * Setter de apellido
+     * @param apellido a modificar
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    /**
+     * Setter de edad
+     * @param edad a modificar
+     */
+    public void setEdad(byte edad) {
+        this.edad = edad;
+    }
+    
+    
+    
     
 }
--- GestorBibliotecaProyecto/src/main/java/objetosVivos/Usuario.java
+++ GestorBibliotecaProyecto/src/main/java/objetosVivos/Usuario.java
@@ -9,6 +9,30 @@ package objetosVivos;
  *
  * @author nicoc
  */
-public class Usuario {
+public class Usuario extends Persona {
+    private boolean estado;//Si esta dado de alta o esta de baja 
+    
+    /**
+     * Constructor de Usuario con los parametros de Persona
+     * @param n nombre del usuario
+     * @param a apellido del usuario
+     * @param e edad del usuario
+     * @param d dni del usuario
+     */
+    public Usuario(String n, String a, byte e, String d) {
+        super(n, a, e,d);
+        
+    }
+
+    
+    /**
+     * Getter de estado
+     * @return el estado true si esta dado de alta y false si esta dado de baja
+     */
+    public boolean isEstado() {
+        return estado;
+    }
+    
+    
     
 }
